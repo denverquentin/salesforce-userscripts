97e251543a24344a40a59e0b1f2cef9f48388237
Quentin Fish <quentinf@gmail.com>
2018-02-26 17:11:57 -0700

    added back VF Pages & fixed SF url change for visualforce.com

--- adminLinks-classic.js
+++ adminLinks-classic.js
@@ -3,7 +3,7 @@
 // @description	  adds handy setup links to the top of all classic Salesforce pages
 // @author        quentin fish
 // @include       https://*.salesforce.com/*
-// @include       https://*.visual.force.com/*
+// @include       https://*.visualforce.com/*
 // ==/UserScript==
 
 var betterSetupLinks = '<a href="/setup/forcecomHomepage.apexp" style="padding:0px;margin:0px;">Setup</a>&nbsp;|&nbsp;' +
@@ -12,14 +12,11 @@ var betterSetupLinks = '<a href="/setup/forcecomHomepage.apexp" style="padding:0
 '<a href="/setup/ui/listApexTraces.apexp" style="padding:0px;margin:0px;">Logs</a>&nbsp;|&nbsp;' +
 '<a href="/01p" style="padding:0px;margin:0px;">Classes</a>&nbsp;|&nbsp;' +
 '<a href="/ui/setup/apex/ApexTestQueuePage" style="padding:0px;margin:0px;">Tests</a>&nbsp;|&nbsp;' +
+'<a href="/apexpages/setup/listApexPage.apexp" style="padding:0px;margin:0px;">Pages</a>&nbsp;|&nbsp;' +
+'<a href="/0DM/o?setupid=CustomDomain" style="padding:0px;margin:0px;">Sites</a>&nbsp;|&nbsp;' +
 '<a href="/apexpages/setup/listAsyncApexJobs.apexp" style="padding:0px;margin:0px;">Jobs</a>&nbsp;|&nbsp;' +
 '<a href="/08e" style="padding:0px;margin:0px;">Sched</a>&nbsp;|&nbsp;' +
 '<a href="/setup/ui/listCustomSettings.apexp" style="padding:0px;margin:0px;">CS</a>&nbsp;|&nbsp;' +
-/*	don't use these much - removing to free up space
-'<a href="/0DM/o?setupid=CustomDomain" style="padding:0px;margin:0px;">Sites</a>&nbsp;|&nbsp;' +
-'<a href="/changemgmt/listOutboundChangeSet.apexp" style="padding:0px;margin:0px;">Out</a>&nbsp;|&nbsp;' +
-'<a href="/changemgmt/listInboundChangeSet.apexp" style="padding:0px;margin:0px;">In</a>&nbsp;|&nbsp;' +
-*/
 '<a href="/005?isUserEntityOverride=1&retURL=%2Fui%2Fsetup%2FSetup%3Fsetupid%3DUsers&setupid=ManageUsers" style="padding:0px;margin:0px;">Users</a>&nbsp;|&nbsp;' +
 '<a href="/00e?setupid=EnhancedProfiles" style="padding:0px;margin:0px;">Profiles</a>&nbsp;|&nbsp;' +
 '<a href="/ui/setup/Setup?setupid=PersonalSetup" style="padding:0px;margin:0px;">MS</a>&nbsp;|&nbsp;' +
--- adminLinks-classicCommunities.js
+++ adminLinks-classicCommunities.js
@@ -3,27 +3,27 @@
 // @description	  adds handy setup links to the top of all classic Communities Salesforce pages
 // @author        quentin fish
 // @include       https://*.salesforce.com/*
-// @include       https://*.visual.force.com/*
+// @include       https://*.visualforce.com/*
 // ==/UserScript==
 
-var betterSetupLinks = '&nbsp;&nbsp;&nbsp;&nbsp;<a href="/setup/forcecomHomepage.apexp" style="color:#fff;">Setup</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/01p" style="color:#fff;">Classes</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/ui/setup/apex/ApexTestQueuePage" style="color:#fff;">Tests</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a target="_blank" href="/_ui/common/apex/debug/ApexCSIPage" style="color:#fff;">Console</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/setup/ui/listApexTraces.apexp" style="color:#fff;">Logs</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/apexpages/setup/listAsyncApexJobs.apexp" style="color:#fff;">Jobs</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/apexpages/setup/listApexPage.apexp" style="color:#fff;">Pages</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/setup/ui/listCustomSettings.apexp" style="color:#fff;">CS</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/0DM/o?setupid=CustomDomain" style="color:#fff;">Sites</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/changemgmt/listOutboundChangeSet.apexp" style="color:#fff;">Out</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/changemgmt/listInboundChangeSet.apexp" style="color:#fff;">In</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/p/setup/custent/CustomObjectsPage" style="color:#fff;">Objects</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/005" style="color:#fff;">Users</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/00e?setupid=EnhancedProfiles" style="color:#fff;">Profiles</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a href="/ui/setup/Setup?setupid=PersonalSetup" style="color:#fff;">MS</a>&nbsp;&nbsp;|&nbsp;&nbsp;' +
-'<a target="_blank" href="https://help.salesforce.com/home" style="padding:0px;margin:0px;">Help</a>&nbsp;|&nbsp;' +
-'<a href="/lightning/switcher?destination=lex" style="padding:0px;margin:0px;">Lightning</a>&nbsp;|&nbsp;' +
-'<a href="/secur/logout.jsp" style="color:#fff;">Logout</a>&nbsp;&nbsp;&nbsp;&nbsp;';
+var betterSetupLinks = '&nbsp;&nbsp;<a href="/setup/forcecomHomepage.apexp" style="color:#fff;">Setup</a>&nbsp;|&nbsp;' +
+'<a href="/p/setup/custent/CustomObjectsPage" style="color:#fff;">Objects</a>&nbsp;|&nbsp;' +
+'<a target="_blank" href="/_ui/common/apex/debug/ApexCSIPage" style="color:#fff;">Console</a>&nbsp;|&nbsp;' +
+'<a href="/setup/ui/listApexTraces.apexp" style="color:#fff;">Logs</a>&nbsp;|&nbsp;' +
+'<a href="/01p" style="color:#fff;">Classes</a>&nbsp;|&nbsp;' +
+'<a href="/ui/setup/apex/ApexTestQueuePage" style="color:#fff;">Tests</a>&nbsp;|&nbsp;' +
+'<a href="/apexpages/setup/listApexPage.apexp" style="color:#fff;">Pages</a>&nbsp;|&nbsp;' +
+'<a href="/0DM/o?setupid=CustomDomain" style="color:#fff;">Sites</a>&nbsp;|&nbsp;' +
+'<a href="/apexpages/setup/listAsyncApexJobs.apexp" style="color:#fff;">Jobs</a>&nbsp;|&nbsp;' +
+'<a href="/08e" style="color:#fff;">Sched</a>&nbsp;|&nbsp;' +
+'<a href="/setup/ui/listCustomSettings.apexp" style="color:#fff;">CS</a>&nbsp;|&nbsp;' +
+'<a href="/setup/ui/listCustomSettings.apexp" style="color:#fff;">CS</a>&nbsp;|&nbsp;' +
+'<a href="/005" style="color:#fff;">Users</a>&nbsp;|&nbsp;' +
+'<a href="/00e?setupid=EnhancedProfiles" style="color:#fff;">Profiles</a>&nbsp;|&nbsp;' +
+'<a href="/ui/setup/Setup?setupid=PersonalSetup" style="color:#fff;">MS</a>&nbsp;|&nbsp;' +
+'<a target="_blank" href="https://help.salesforce.com/home" style="color:#fff;">Help</a>&nbsp;|&nbsp;' +
+'<a href="/lightning/switcher?destination=lex" style="color:#fff;">Lightning</a>&nbsp;|&nbsp;' +
+'<a href="/secur/logout.jsp" style="color:#fff;">Logout</a>&nbsp;&nbsp;';
 
 var globalHeaderBar = document.getElementById("globalHeaderBar");
 if (globalHeaderBar != null) {
