692a77517f68e128ac0632a00399031fdb756b7a
Quentin <denverquentin@users.noreply.github.com>
2025-01-29 15:20:22 -0700

    tweak for lightning

--- adminLinks-lightning.js
+++ adminLinks-lightning.js
@@ -4,6 +4,7 @@
 // @version       1
 // @author        quentin fish
 // @match         https://*.lightning.force.com/*
+// @match         https://*.salesforce-setup.com/lightning/*
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js
 // @require       https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant         GM_addStyle
@@ -11,18 +12,18 @@
 
 
 function addSetupLinks(jNode) {
-/* these are lightning setup urls:
-setup home - setup/one/one.app#/setup/home
-apex classes - /one/one.app?source=aloha#/setup/ApexClasses/home
-debug logs - /one/one.app?source=aloha#/setup/ApexDebugLogs/home
-apex jobs - /one/one.app?source=aloha#/setup/AsyncApexJobs/home
-vf pages - /one/one.app?source=aloha#/setup/ApexPages/home
-custom settings - /one/one.app?source=aloha#/setup/CustomSettings/home
-users - /one/one.app?source=aloha#/setup/ManageUsers/home
-profiles - /one/one.app?source=aloha#/setup/EnhancedProfiles/home
-
-NOT USING lighting setup urls above YET because it SUCKS
-*/
+	/* these are lightning setup urls:
+	setup home - setup/one/one.app#/setup/home
+	apex classes - /one/one.app?source=aloha#/setup/ApexClasses/home
+	debug logs - /one/one.app?source=aloha#/setup/ApexDebugLogs/home
+	apex jobs - /one/one.app?source=aloha#/setup/AsyncApexJobs/home
+	vf pages - /one/one.app?source=aloha#/setup/ApexPages/home
+	custom settings - /one/one.app?source=aloha#/setup/CustomSettings/home
+	users - /one/one.app?source=aloha#/setup/ManageUsers/home
+	profiles - /one/one.app?source=aloha#/setup/EnhancedProfiles/home
+	
+	NOT USING lighting setup urls above YET because it SUCKS
+	*/
 	jNode.prepend (
 		'<span style="color:#000;font-weight:bold;padding:0px;margin-top:3px;"><a href="/setup/forcecomHomepage.apexp" style="color:#000;">Setup</a>&nbsp;|&nbsp;' +
 		'<a href="/p/setup/custent/CustomObjectsPage" style="color:#000;">Objects</a>&nbsp;|&nbsp;' +
